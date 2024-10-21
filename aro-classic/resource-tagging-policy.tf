8cce9a93d8b99b64068774eea184c1d51d2404ec
Luqman Barry <lbarry@redhat.com>
2024-10-21 14:35:07 -0500

    Added Day2 GitOps architecture

--- aro-classic/resource-tagging-policy.tf
+++ aro-classic/resource-tagging-policy.tf
@@ -1,13 +1,13 @@
-# resource "null_resource" "run_always" {
-#   provisioner "local-exec" {
-#     interpreter = [ "/bin/bash", "-c" ]
-#     command = "echo 'Always run!'"
-#   }
+resource "null_resource" "run_always" {
+  provisioner "local-exec" {
+    interpreter = [ "/bin/bash", "-c" ]
+    command = "echo 'Always run!'"
+  }
 
-#   triggers = {
-#     timestamp = "${timestamp()}"
-#   }
-# }
+  triggers = {
+    timestamp = "${timestamp()}"
+  }
+}
 
 resource "azurerm_policy_definition" "rg_tagging_policy_definition" {
 
@@ -668,7 +668,7 @@ resource "azurerm_policy_definition" "rg_tagging_policy_definition" {
       condition     = length(local.resource_tags) <= 18
       error_message = "The policy does not support more than 18 tags. Reduce the num of tags and try again."
     }
-    # replace_triggered_by = [ null_resource.run_always ]
+    replace_triggered_by = [ null_resource.run_always ]
   }
 }
 
@@ -707,7 +707,7 @@ resource "azurerm_subscription_policy_assignment" "rg_tagging_policy_assignment"
       condition     = length(local.resource_tags) <= 18
       error_message = "The policy does not support more than 18 tags. Reduce the num of tags and try again."
     }
-    # replace_triggered_by = [ null_resource.run_always ]
+    replace_triggered_by = [ null_resource.run_always ]
   }
 }
 
